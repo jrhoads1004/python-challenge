e2bd32415418b3ea6ba6b142cfcebc1fcd47cc0f
Joshua Rhoads <josh@Joshuas-MacBook-Pro.local>
2020-08-24 08:09:50 -0500

    changed code to print to terminal

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,22 @@
 import os 
 import csv
 
-PyBank = os.path.join('..', 'Resources', 'budget_data.csv')
+PyBank = os.path.join("Resources", "budget_data.csv")
 
 change = []
 months = []
 profit = []
 
-with open("budget_data.csv", "r") as csvfile:
+with open(PyBank, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     header = next(csvreader)
     
     for row in csvreader:
         months.append(row[0])
-        profits.append(row[1])
+        profit.append(row[1])
        
     total_months = len(months)
-    total_profit = sum(profit)
+    total_profit = str(profit)
 
     for i in range(1, len(profit)):
         change.append((int(profit[i]) - int(profit[i-1])))
@@ -30,20 +30,21 @@ print("Financial Analysis")
 print("-----------------------------")
 print("Total months: " + str(total_months))
 print("Total profits: " + str(profit)) 
-print("Average change: " + str(average_change))
-print("Greatest Increase in Profits: " + str(greatest_profit_increase_index))
-print("Greatest Decrease in Profits: " + str(greatest_profit_decrease_index))
-text_export = os.path.join(`budget_data.csv`)
+print("Average change: $" + str(average_change))
+print("Greatest Increase in Profits: $" + str(greatest_profit_increase_index))
+print("Greatest Decrease in Profits: $" + str(greatest_profit_decrease_index))
 
-with open("Bank_Analysis.txt", "w") as txt_file:
-    csv_writer = csv.writer(txt_file)
 
-csv_writer.writerow("Financial Analysis")
-csv_writer.writerow("---------------------------")
-   # csv_writer.writerow("Total months: {len(months)-1:,])
-   # csv_writer.writerow([f"Total profits: ${total_profit:,}"])
-    #csv_writer.writerow([f"Average Change: ${average_change:,}"])
-   # csv_writer.writerow([f"Greatest Increase in Profits: {months[offset_greatest_increase]} (${max(changes):,})"])
-    #csv_writer.writerow([f"Greatest Decrease in Profits: {months[offset_greatest_decrease]} (${mix(changes):,})"])
+#with open("Bank_Analysis.txt", "w") as txt_file:
+    #csv_writer = csv.writer(txt_file)
+
+#csv_writer.writerow(["Financial Analysis"])
+#csv_writer.writerow(["---------------------------"])
+#csv_writer.writerow("Total months: " + str(total_months))
+#csv_writer.writerow("Total profits: " + str(profit))
+#csv_writer.writerow("Average change: " + str(average_change))
+#csv_writer.writerow("Greatest Increase in Profits: " + str(greatest_profit_increase_index))
+#csv_writer.writerow("Greatest Decrease in Profits: " + str(greatest_profit_decrease_index))
+
 
 
