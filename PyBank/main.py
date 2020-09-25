7df1f737dbd60d79249d79d4dd25875a88efa7b2
Joshua Rhoads <josh@Joshuas-MacBook-Pro.local>
2020-09-25 05:20:42 -0500

    final output code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,51 +1,68 @@
 import os 
 import csv
 
-csvpath = os.path.join("Resources/budget_data.csv")
+month_count = 0
+total_sum = 0
+inc_max_month = ""
+dec_min_month = ""
+inc_max = 0
+dec_min = 0
+change = 0
+st_month = 0
+nd_month = 0
 
-change = []
-months = []
-profit = []
+csvpath = os.path.join("PyBank", "Resources", "budget_data.csv")
 
-with open(csvpath, "r") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter= ",")
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
-    
+
     for row in csvreader:
-        months.append(row[0])
-        profit.append(row[1])
-       
-    total_months = len(months)
-    total_profit = len(profit)
 
-    for i in range(1, len(profit)):
-        change.append((int(profit[i]) - int(profit[i-1])))
+        month_count += 1
+        total_sum += int(row[1]) # Count for set of data
 
-average_change = sum(change) / len(change)
+        # Count the change for every month
+        if month_count == 1:
+            st_month = int(row[1])
+        else:
+            nd_month = int(row[1])
+            change += nd_month - st_month
+            st_month = nd_month
 
-greatest_profit_increase_index = max(change)
-greatest_profit_decrease_index = min(change)
+        if int(row[1]) > inc_max:
+            
+            inc_max = int(row[1])
+            inc_max_month = row[0] # find the greatest increase 
 
+        elif int(row[1]) < dec_min:
+
+            dec_min = int(row[1])
+            dec_min_month = row[0] # find the greatest decrease
+
+avg_change = change / (month_count - 1)
+#print out the result    
 print("Financial Analysis")
-print("-----------------------------")
-print("Total months: " + str(total_months))
-print("Total profits: " + str(profit)) 
-print("Average change: $" + str(average_change))
-print("Greatest Increase in Profits: $" + str(greatest_profit_increase_index))
-print("Greatest Decrease in Profits: $" + str(greatest_profit_decrease_index))
+print("---------------------------------")
+print("Total months: " + str(month_count))
+print("Total: $" + str(total_sum))
+print("Average Change: $" + str(round(avg_change, 2)))
+print("Greatest Increase in Profits: " + inc_max_month + " ($" + str(inc_max) + ")")
+print("Greatest Decrease in Profits: " + dec_min_month + " ($" + str(dec_min) + ")")
 
-output_file = os.path.join("Resources/Bank_Analysis.txt")
+#export the result into a new csv file
+op_path = os.path.join("finacial_analysis.csv")
 
-with open(output_file, "w") as txt_file:
-    csv_writer = csv.writer(txt_file)
+with open(op_path, "w", newline = "", encoding = "UTF-8") as datafile:
 
-csv_writer.writerow("Financial Analysis")
-csv_writer.writerow("---------------------------")
-csv_writer.writerow("Total months: " + str(total_months))
-csv_writer.writerow("Total profits: " + str(profit))
-csv_writer.writerow("Average change: " + str(average_change))
-csv_writer.writerow("Greatest Increase in Profits: " + str(greatest_profit_increase_index))
-csv_writer.writerow("Greatest Decrease in Profits: " + str(greatest_profit_decrease_index))
+    writer = csv.writer(datafile, delimiter=',')
 
+    writer.writerow(["Financial Analysis"])
+    writer.writerow(["Totla months", month_count])
+    writer.writerow(["Total", total_sum])
+    writer.writerow(["Average Change",round(avg_change, 2)])
+    writer.writerow(["Greatest Increase in Profits",inc_max_month,inc_max])
+    writer.writerow(["Greatest Decrease in Profits",dec_min_month,dec_min])
 
 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # python-challenge
-profit/losses of company &amp; election vote data
+
